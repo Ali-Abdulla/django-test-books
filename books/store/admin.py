7364ee77bf2a0a14d901955f65d8e84eb5adf40b
ali <sashaal-sayed@MacBook-Pro-Sasha.local>
2021-10-01 00:26:33 +0300

    Initial commit

---
+++ books/store/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from django.contrib.admin import ModelAdmin
+
+from store.models import Book
+
+
+@admin.register(Book)
+class BookAdmin(ModelAdmin):
+    pass
+
